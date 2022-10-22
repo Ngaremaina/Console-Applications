ceffa45d05d1136a6aaf92c034b13a119370bef6
Ngare <45742522+Ngaremaina@users.noreply.github.com>
2022-10-22 18:23:41 +0300

    Data Structures

---
+++ Data Structures/Power.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x, n;
+
+    if (n<0){
+        cout<<"n must be a positive integer";
+    }
+    else{
+        while (n>0){
+        result = x**n;
+        cout<<result<<endl;
+    }
+    }
+
+    
+}
