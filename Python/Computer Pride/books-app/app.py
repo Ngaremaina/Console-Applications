860161d43c541361b2b0c2bf2a04645c31276d85
Myner <45742522+Ngaremaina@users.noreply.github.com>
2023-02-15 16:25:12 +0300

    Python Programming

--- Python/Computer Pride/books-app/app.py
+++ Python/Computer Pride/books-app/app.py
@@ -1,6 +1,10 @@
 from utils import database
 
-USER_CHOICE = """
+# all_books = database.books
+
+# print(all_books)
+
+USER_CHOICE ="""
 Enter:
 - 'a' to add a new book
 - 'l' to list all the books
@@ -8,33 +12,81 @@ Enter:
 - 'd' to delete a book
 - 'q' to quit
 
-Enter Your Your Choice: """
-
-# the menu
+Enter your choice: """
+#the menu
 def menu():
     user_input = input(USER_CHOICE)
-    # deal with the user's input choice
+    #deal with the user input choice
     while user_input != 'q':
         if user_input == 'a':
             prompt_add_book()
-    
-# create a function to ask the user for the book name and author
+        elif user_input == 'l':
+            list_books()
+        elif user_input == 'd':
+            prompt_delete_book()
+        elif user_input == 'r':
+            prompt_read_book()
+        else:
+            print("Invalid choice, Please try again!!!")
+        user_input = input(USER_CHOICE)
+            
+#create a function to ask the user for the book name and author
 def prompt_add_book():
     name = input("Enter the name of the new book: ")
     author = input("Enter the author of the new book: ")
     database.add_book(name, author)
-
-# a function to show all the books in our list
+    
+#a function to show all the books in our list
+# def list_books():
+#     for book in database.books:
+#             database.display(book)  
+            
 def list_books():
-    pass
-
-# a function that asks the user for book name and changes it to 'read' in our list
+    # all_books = database.display()
+    # s = "" if len(all_books) == 1 else "s"
+    # print(f"We have {len(all_books)} book{s} at the moment.")
+    # # for book in all_books:
+    # #     print(f"Book Title: {book['name']}, Written By: {book['author']}, Read Status is {book['read']}")
+    # for index, book in enumerate(all_books):
+    #     status = "Yes" if book['read'] == True else "No"
+    #     print(f"{index + 1}. Book Title: {book['name']}, Written By: {book['author']}, Read Status is {status}")
+    # s = "" if len(all_books) == 1 else "s"
+    # if len(all_books) > 0:
+    #     print("We have {len(all_books)} book{s} at the moment.")
+    # else:
+    #     print("We have {len(all_books)} at the moment. Come back later")
+    # all_books = database.display()
+    # for book in all_books:
+    #     name = book[0]
+    #     author = book[1]
+    #     read = book[2]
+    #     status = "Yes" if read == '1' else "No"
+    #     # if read == 0:
+    #     #     read = "Yes"
+    #     # else:
+    #     #     read = "No"
+    #     # print(f"{name}, Written By: {author}, Read Status {read}")
+    #     print(f"{name}, Written By: {author}, Read Status {status}")
+    all_books = database.get_all_books()
+    # print(all_books)
+    for book in all_books:
+        # print(book)
+        read = "Yes" if book['read']== '1' else "No"
+        print(f"{book['name']}, written by{book['author']}, read staus:{read}")
+    
+# def list_books():
+#     database.display()
+             
+#a function that asks the user for book name and changes it to 'read in out list
 def prompt_read_book():
-    pass
-
-# a function thats asks for book name and removesit from our list
+     name = input("Enter the name of the new book: ")
+     database.markbookasread(name)
+            
+             
+#a function that asks for book name and removes it from our list
 def prompt_delete_book():
-    pass 
+    name = input("Enter the name of the book: ")
+    database.delete_book(name)
+               
 
-# call our menu function
 menu()
--- Python/Computer Pride/books-app/utils/database.py
+++ Python/Computer Pride/books-app/utils/database.py
@@ -1,13 +1,84 @@
-# create our empty list to store our books
-books = []
-
-# create a function that adds a book to our list
+#create our empty list to store our books
+# books = []
+books = "books.txt"
+file_path = "books.txt"
+#create a function that adds a book to our list
 def add_book(name, author):
-    books.append({
-        "name": name,
-        "author": author,
-        "read": False
-    })
+    # books.append({
+    #     "name":name,
+    #     "author": author,
+    #     "read": False
+        
+    # })
+    
+    with open(file_path, 'a') as file:
+        file.write(f"{name}, {author},0\n")
+        print(f"Book with the name {name}, created successfully!!")
+     
+   
+    
+#create a function that shows/displays all the books to the user
+# def display(book):
+#     print(f"Book Title: {book['name']}, Written By: {book['author']}, Read Status is {book['read']}")
+    
+    
+def get_all_books():
+    # for book in books:
+    #     print(f"Book Title: {book['name']}, Written By: {book['author']}, Read Status is {book['read']}")
+    with open(file_path, 'r') as file:
+        # content = file.read()
+        all_books = [book.strip("\n").split(",") for book in file.readlines()]
+        # return all_books
+        return [
+            {"name": book[0],"author":book[1],"read":book[2]} for book in all_books               
+                
+        ]
+            # print(file.read())
+    # return books
+          
+            
+#create a function that marks a book as read -> changes False to True
+def markbookasread(book_name):
+    # for book in books:
+    #     if name == book['name']:
+    #         book['read'] = True
+    #         display(book)
+    # with open(file_path, 'a') as file:
+    #     all_books = display()
+    #     for book in all_books:
+    #         name = book[0]
+    #         author = book[1]
+    #         read=book[2]
+    #         if name.lower()==book_name.lower():
+    #             read = '1'
+    #             file.write(f"{name},{author},{read}")
+                # book[2] = '1'
+        # return all_books
+    books = get_all_books()
+    for book in books:
+        if book['name'] == book_name:
+            book['read'] =="1"
+            
+    _save_all_books(books)
+                
+#create a function to save all books
+def _save_all_books(books):
+    with open(file_path, 'w') as f:
+        for book in books:
+            f.write(f"{book['name']} written by {book['author']} read status{book['read']}\n")
 
-# create a function that shoes/displays all the books to the user
 
+
+def delete_book(name):
+    # for book in books:
+    #     if name == book['name']:
+    #         books.remove(book)
+    #         print(f"The book {book} has been deleted")
+    
+    # global books
+    # books = [book for book in books if name.lower() != book['name'].lower()]
+    # books.remove()
+    # 
+    books = get_all_books() 
+    books = [book for book in books if book['name'] != name]
+    _save_all_books(books)
